0d087b010c25d0ff507204bbe72c5d898f691167
mluimes <mario.luis.mesa2004@gmail.com>
2025-12-09 20:08:58 +0100

    feat: vampire Shapeshift working

---
+++ Assets/[Venator]/[Vampire]/ShapeState.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// Represents the shape state of the Vampire
+/// </summary>
+public enum ShapeState
+{
+    Vampire,
+    Bat
+}
---
+++ Assets/[Venator]/[Vampire]/ShapeshiftManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ShapeshiftManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] GameObject vampireForm;
+    [SerializeField] GameObject batForm;
+    [SerializeField] ParticleSystem shapeshiftPS;
+
+    [Header("Shapeshift Variables")]
+    [SerializeField] int shapeshiftDelayMs = 600; // its in milliseconds
+    public ShapeState currentForm;
+
+
+    void Start()
+    {
+        if (vampireForm == null) Debug.LogError("vampireForm not assigned in the inspector");
+        if (batForm == null) Debug.LogError("batForm not assigned in the inspector");
+        if (shapeshiftPS == null) Debug.LogError("shapeshiftPS not assigned in the inspector");
+    }
+
+    public async void Shapeshift()
+    {
+        shapeshiftPS.Play();
+
+        await Task.Delay(shapeshiftDelayMs);
+
+        if (currentForm == ShapeState.Vampire)
+        {
+            vampireForm.SetActive(false);
+            batForm.SetActive(true);
+            currentForm = ShapeState.Bat;
+        }
+        else
+        {
+            batForm.SetActive(false);
+            vampireForm.SetActive(true);
+            currentForm = ShapeState.Vampire;
+        }
+    }
+}
