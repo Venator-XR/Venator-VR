c01f6fc3b06713244de30e15fbf0566547e61994
Boregxrd <manuelborregales12@gmail.com>
2026-01-09 12:59:17 +0100

    feat: Introduce global state for checkpoint spawn preference

---
+++ Assets/[Venator]/Castle/Checkpoints/CheckpointState.cs
@@ -0,0 +1,7 @@
+public static class GameState
+{
+    // This is just data storage as a global variable
+    // It will be used to determine if the player should spawn at the checkpoint or
+    // at the starting spawn point.
+    public static bool SpawnAtCheckpoint = false;
+}
