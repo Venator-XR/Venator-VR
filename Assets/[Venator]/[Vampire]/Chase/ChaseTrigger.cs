89ed33e9e18b2629c8205744191b180cdb3d5f2a
mluimes <mario.luis.mesa2004@gmail.com>
2026-01-09 20:23:32 +0100

    fix: ChaseTrigger can't retrigger no more

--- Assets/[Venator]/[Vampire]/Chase/ChaseTrigger.cs
+++ Assets/[Venator]/[Vampire]/Chase/ChaseTrigger.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ChaseTrigger : MonoBehaviour
 {
     public VampireChaseManager vampireChaseManager;
+    public bool approachStarted = false;
 
     void OnTriggerEnter(Collider other)
     {
+        if (approachStarted) return;
         if(other.CompareTag("Player"))
         {
+            approachStarted = true;
             vampireChaseManager.StartApproach();
         }
     }
