5e7df8fbc15265c02412e901915bf135d359ea99
mluimes <mario.luis.mesa2004@gmail.com>
2026-01-10 23:26:29 +0100

    feat: BatAttack sfx

--- Assets/[Venator]/[Vampire]/BatAttack/BatAttackTrigger.cs
+++ Assets/[Venator]/[Vampire]/BatAttack/BatAttackTrigger.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class BatAttackTrigger : MonoBehaviour
 {
+    [Header("Refs")]
     [SerializeField] private GameObject batAttackPrefab;
     [SerializeField] private Transform batStartTransform;
     [SerializeField] private Transform objectiveTransform;
+
+    [Header("SFXs")]
+    [SerializeField] private AudioClip attackSFX;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            other.GetComponent<AudioSource>().PlayOneShot(attackSFX);
             Quaternion rotation = Quaternion.LookRotation(objectiveTransform.position - batStartTransform.position);
             Instantiate(batAttackPrefab, batStartTransform.position, rotation);
             Destroy(gameObject);
