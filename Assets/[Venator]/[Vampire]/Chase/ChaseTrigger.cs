aeb6f4a8bcf86df857fb638e059ce1463c0762bf
mluimes <mario.luis.mesa2004@gmail.com>
2026-01-10 18:54:07 +0100

    fix: ChaseTrigger Destroy instead of return

--- Assets/[Venator]/[Vampire]/Chase/ChaseTrigger.cs
+++ Assets/[Venator]/[Vampire]/Chase/ChaseTrigger.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 public class ChaseTrigger : MonoBehaviour
 {
     public VampireChaseManager vampireChaseManager;
-    public bool approachStarted = false;
 
     void OnTriggerEnter(Collider other)
     {
-        if (approachStarted) return;
         if(other.CompareTag("Player"))
         {
-            approachStarted = true;
             vampireChaseManager.StartApproach();
+            Destroy(gameObject);
         }
     }
 }
